3f6ffd5f54cc8965c41aa7ea67c33b5e0f8fa736
kyulyak <yulia.kirkina@mail.ru>
2025-04-08 20:11:14 +0300

    addition of reader and writer and input data validation

--- lib/rectangle_integrator/rectangle_integrator.rb
+++ lib/rectangle_integrator/rectangle_integrator.rb
@@ -5,22 +5,42 @@ require_relative "rectangle_integrator/version"
 module RectangleIntegrator
   class Error < StandardError; end
   class RectangleIntegrator
+    attr_reader :func, :a, :b
+    
     def initialize(func, a, b)
       @func = func
       @a = a
       @b = b
+      validate_interval
+    end
+    
+    def left_rectangle
+      (@b - @a) * func.call(a)
     end
   
-    def left_rectangle
-      (@b - @a)* @func.call(@a)
-    end
-
     def right_rectangle
-      (@b - @a)* @func.call(@b)
+      (@b - @a) * func.call(b)
     end
-
+  
     def average_rectangle
-      (@b - @a)* @func.call((@a+@b)/2)
+      midpoint = (a + b) / 2.0
+      (@b - @a) * func.call(midpoint)
+    end
+    
+    def a=(new_a)
+      @a = new_a
+      validate_interval
+    end
+    
+    def b=(new_b)
+      @b = new_b
+      validate_interval
+    end
+    
+    def validate_interval
+      if a >= b
+        raise ArgumentError, "Wrong interval 'a' must be less then 'b'"
+      end
     end
   end
 end
