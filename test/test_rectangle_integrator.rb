6278419cb2e19030db3dfc29b8f878c89cad02a9
VladimitGrushin <klasniy.chuvachok@gmail.com>
2025-04-08 20:31:15 +0300

    add tests for functions

--- test/test_rectangle_integrator.rb
+++ test/test_rectangle_integrator.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
+require "minitest/autorun"
+#require 'test/unit/ui/console/testrunner'
+require_relative "lib/rectangle_integrator"
 class TestRectangleIntegrator < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::RectangleIntegrator::VERSION
@@ -10,4 +12,27 @@ class TestRectangleIntegrator < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def test_left_rectangle
+    integrator = RectangleIntegrator.new(->(x) { x**2 }, 0, 1)
+    result = integrator.left_rectangle
+    assert_equal  0, result
+  end
+
+  def test_right_rectangle
+    integrator = RectangleIntegrator.new(->(x) { x**2 }, 0, 1)
+    result = integrator.right_rectangle
+    assert_equal  1, result
+  end
+
+  def test_average_rectangle
+    integrator = RectangleIntegrator.new(->(x) { x**2 }, 0, 1)
+    result = integrator.average_rectangle
+    assert_equal  0.25, result
+  end
+
+  def test_zero_interval
+    integrator = RectangleIntegrator.new(-> (x) {x}, 0,0)
+    result = integrator.average_rectangle
+    assert_equal 0, result
 end
