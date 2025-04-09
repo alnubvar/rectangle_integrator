205992263f9f50fabf703b10890bf5d9f9a6a69a
alnubvar <alnub_work@mail.ru>
2025-04-09 09:50:56 +0300

    add method right rectangle for error estimation

--- lib/error_estimator.rb
+++ lib/error_estimator.rb
@@ -12,5 +12,15 @@ module RectangleIntegrator
       end.map(&:abs).max
       ((b - a)**2 / (2.0 * n)) * max_derivative
     end
+
+    def self.right_rectangle_error(f, a, b, n, samples: 1000)
+      h = (b - a) / samples.to_f
+      max_derivative = (0..samples).map do |i|
+        x = a + i * h
+        (f.call(x + 1e-5) - f.call(x - 1e-5)) / (2e-5)
+      end.map(&:abs).max
+      ((b - a)**2 / (2.0 * n)) * max_derivative
+    end
+
   end
 end
