c104f9accd418dd24342d1243e60dae1f101253a
alnubvar <alnub_work@mail.ru>
2025-04-09 09:49:34 +0300

    add method left rectangle for error estimation

---
+++ lib/error_estimator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+
+module RectangleIntegrator
+  module ErrorEstimator
+
+    def self.left_rectangle_error(f, a, b, n, samples: 1000)
+      h = (b - a) / samples.to_f
+      max_derivative = (0..samples).map do |i|
+        x = a + i * h
+        (f.call(x + 1e-5) - f.call(x - 1e-5)) / (2e-5)
+      end.map(&:abs).max
+      ((b - a)**2 / (2.0 * n)) * max_derivative
+    end
+  end
+end
