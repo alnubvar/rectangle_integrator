05e51884718d843f485ff8b4dd28c0bbda1dfb64
alnubvar <alnub_work@mail.ru>
2025-04-09 09:52:03 +0300

    add method midpoint rectangle for error estimation

--- lib/error_estimator.rb
+++ lib/error_estimator.rb
@@ -22,5 +22,13 @@ module RectangleIntegrator
       ((b - a)**2 / (2.0 * n)) * max_derivative
     end
 
+    def self.midpoint_rectangle_error(f, a, b, n, samples: 1000)
+      h = (b - a) / samples.to_f
+      max_second_derivative = (0..samples).map do |i|
+        x = a + i * h
+        (f.call(x - 1e-5) - 2 * f.call(x) + f.call(x + 1e-5)) / (1e-5**2)
+      end.map(&:abs).max
+      ((b - a)**3 / (24.0 * n**2)) * max_second_derivative
+    end
   end
 end
